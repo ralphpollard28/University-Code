9e0c720e60c460b182c0af40b20db9134923086e
Ralph <ralphpollard28@gmail.com>
2025-10-14 14:34:23 +0100

    Added week 1 and 2 to portfolio

---
+++ week1_lab.py
@@ -0,0 +1,10 @@
+print("Hell, World")
+student_name = "Ralph"
+Student_age = 18
+print("student Name:", student_name)
+print("student age:", Student_age)
+
+num1 = 467
+num2 = 41
+total = num1 + num2
+print("The sum is:", total)
